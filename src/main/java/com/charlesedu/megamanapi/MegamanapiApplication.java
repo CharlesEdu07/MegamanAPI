fc494bfc204b942e045e27c58a8e2fa672604b47
unknown <charleseduardofaria@gmail.com>
2023-09-04 08:02:46 -0300

    Criando projeto

---
+++ src/main/java/com/charlesedu/megamanapi/MegamanapiApplication.java
@@ -0,0 +1,13 @@
+package com.charlesedu.megamanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegamanapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegamanapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charlesedu/megamanapi/MegamanapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlesedu.megamanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegamanapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
