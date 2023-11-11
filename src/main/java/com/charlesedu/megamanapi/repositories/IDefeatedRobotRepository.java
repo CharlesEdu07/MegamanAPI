146b01ef195c9e299c6ee06eb321930fa3e7fc47
unknown <charleseduardofaria@gmail.com>
2023-11-10 21:18:42 -0300

    Removendo alguns imports inutilizados

--- src/main/java/com/charlesedu/megamanapi/repositories/IDefeatedRobotRepository.java
+++ src/main/java/com/charlesedu/megamanapi/repositories/IDefeatedRobotRepository.java
@@ -1,7 +1,5 @@
 package com.charlesedu.megamanapi.repositories;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
