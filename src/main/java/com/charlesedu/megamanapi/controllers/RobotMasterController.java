1d0aa1d85a2099c745b93332381345bda2e404a8
unknown <charleseduardofaria@gmail.com>
2023-11-16 22:23:17 -0300

    Terminando funcionalidades básicas da API

--- src/main/java/com/charlesedu/megamanapi/controllers/RobotMasterController.java
+++ src/main/java/com/charlesedu/megamanapi/controllers/RobotMasterController.java
@@ -2,6 +2,8 @@ package com.charlesedu.megamanapi.controllers;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.charlesedu.megamanapi.entities.DefeatedRobot;
 import com.charlesedu.megamanapi.entities.RobotMaster;
+import com.charlesedu.megamanapi.entities.Score;
 import com.charlesedu.megamanapi.repositories.IDefeatedRobotRepository;
 import com.charlesedu.megamanapi.services.RobotMasterService;
 import com.charlesedu.megamanapi.services.exceptions.ResourceNotFoundException;
@@ -54,7 +57,7 @@ public class RobotMasterController {
     }
 
     @GetMapping(value = "/list/{id}")
-    public ResponseEntity<List<DefeatedRobot>> findAllDefeatedRobots(@PathVariable UUID id) {
+    public ResponseEntity<List<Score>> findAllDefeatedRobots(@PathVariable UUID id) {
         RobotMaster robotMaster = service.findById(id);
 
         if (robotMaster == null) {
@@ -64,6 +67,14 @@ public class RobotMasterController {
         List<DefeatedRobot> list = defeatedRobotRepository
                 .findFirst10ById_RobotMasterOrderByDamageTakenAscTimeAsc(robotMaster);
 
-        return ResponseEntity.ok().body(list);
+        List<Score> scores = list.stream()
+                .map(x -> new Score(
+                        x.getRobotList().getUser(),
+                        x.getRobotMaster(),
+                        x.getDamageTaken(),
+                        x.getTime()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(scores);
     }
 }
--- src/main/java/com/charlesedu/megamanapi/entities/Score.java
+++ src/main/java/com/charlesedu/megamanapi/entities/Score.java
@@ -1,31 +1,18 @@
 package com.charlesedu.megamanapi.entities;
 
 import java.io.Serializable;
-import java.util.UUID;
+import java.time.LocalTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "tb_score")
 public class Score implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedValue(generator = "UUID")
-    private UUID id;
-
     private UserModel user;
     private RobotMaster robotMaster;
-    private int damageTaken;
-    private int time;
+    private Integer damageTaken;
+    private LocalTime time;
 
     public Score() {
     }
 
-    public Score(UserModel user, RobotMaster robotMaster, int damageTaken, int time) {
+    public Score(UserModel user, RobotMaster robotMaster, Integer damageTaken, LocalTime time) {
         this.user = user;
         this.robotMaster = robotMaster;
         this.damageTaken = damageTaken;
@@ -48,44 +35,19 @@ public class Score implements Serializable {
         this.robotMaster = robotMaster;
     }
 
-    public int getDamageTaken() {
+    public Integer getDamageTaken() {
         return damageTaken;
     }
 
-    public void setDamageTaken(int damageTaken) {
+    public void setDamageTaken(Integer damageTaken) {
         this.damageTaken = damageTaken;
     }
 
-    public int getTime() {
+    public LocalTime getTime() {
         return time;
     }
 
-    public void setTime(int time) {
+    public void setTime(LocalTime time) {
         this.time = time;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Score other = (Score) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
+    }  
 }
