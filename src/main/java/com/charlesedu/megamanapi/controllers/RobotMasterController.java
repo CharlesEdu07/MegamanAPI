58f93c2e38c1819a4d35c4d4b3bf68f5523acf5a
unknown <charleseduardofaria@gmail.com>
2023-11-26 22:12:15 -0300

    Corrigindo rota de listar robos

--- src/main/java/com/charlesedu/megamanapi/controllers/RobotMasterController.java
+++ src/main/java/com/charlesedu/megamanapi/controllers/RobotMasterController.java
@@ -49,14 +49,14 @@ public class RobotMasterController {
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<RobotMaster> findById(@PathVariable UUID id) {
-        RobotMaster obj = service.findById(id);
+    // @GetMapping(value = "/{id}")
+    // public ResponseEntity<RobotMaster> findById(@PathVariable UUID id) {
+    //     RobotMaster obj = service.findById(id);
 
-        return ResponseEntity.ok().body(obj);
-    }
+    //     return ResponseEntity.ok().body(obj);
+    // }
 
-    @GetMapping(value = "/list/{id}")
+    @GetMapping(value = "{id}")
     public ResponseEntity<List<Score>> findAllDefeatedRobots(@PathVariable UUID id) {
         RobotMaster robotMaster = service.findById(id);
 
