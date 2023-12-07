4b31c1f556fbf8517e4993303a420883eef4aac6
unknown <charleseduardofaria@gmail.com>
2023-12-07 13:48:53 -0300

    addCorsMappings

--- src/main/java/com/charlesedu/megamanapi/config/WebConfig.java
+++ src/main/java/com/charlesedu/megamanapi/config/WebConfig.java
@@ -9,43 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    // @Bean
-    // public WebMvcConfigurer corsConfigurer() {
-    // return new WebMvcConfigurer() {
-    // @Override
-    // public void addCorsMappings(CorsRegistry registry) {
-    // registry.addMapping("/**")
-    // .allowedOrigins("https://megaman-speedrun-81ua.vercel.app",
-    // "http://localhost:3000")
-    // .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE",
-    // "CONNECT",
-    // "OPTIONS")
-    // .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept",
-    // "Authorization",
-    // "Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
-    // "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials")
-    // .exposedHeaders("Authorization")
-    // .exposedHeaders("Access-Control-Allow-Origin",
-    // "Access-Control-Allow-Headers",
-    // "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials")
-    // .maxAge(3600)
-    // .allowCredentials(true);
-    // }
-    // };
-    // }
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://megaman-speedrun-81ua.vercel.app", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT",
-                        "OPTIONS")
-                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization",
-                        "Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
-                        "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials")
-                .exposedHeaders("Authorization")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
-                        "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials")
-                .allowCredentials(true);
+        registry.addMapping("/**");
     }
 }
