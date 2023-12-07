fe313bad6abf054de51ec64d01fab176cb9bd057
unknown <charleseduardofaria@gmail.com>
2023-12-06 23:33:48 -0300

    Alterações no filter e webconfig

--- src/main/java/com/charlesedu/megamanapi/config/WebConfig.java
+++ src/main/java/com/charlesedu/megamanapi/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.charlesedu.megamanapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,7 +19,7 @@ public class WebConfig {
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
                                 HttpMethod.DELETE.name())
-                        .allowedHeaders(HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION);
+                        .allowedHeaders("Content-Type", "Authorization");
             }
         };
     }
--- src/main/java/com/charlesedu/megamanapi/filter/CorsFilter.java
+++ src/main/java/com/charlesedu/megamanapi/filter/CorsFilter.java
@@ -27,7 +27,6 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Max-Age", "180");
         filterChain.doFilter(servletRequest, servletResponse);
