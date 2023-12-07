d86d261dd04f7f0532e1865ab1399b59427a425b
unknown <charleseduardofaria@gmail.com>
2023-12-07 11:52:27 -0300

    Voltando ao Bean webconfig

--- src/main/java/com/charlesedu/megamanapi/config/WebConfig.java
+++ src/main/java/com/charlesedu/megamanapi/config/WebConfig.java
@@ -1,30 +1,30 @@
-// package com.charlesedu.megamanapi.config;
+package com.charlesedu.megamanapi.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// @EnableWebMvc
-// public class WebConfig {
+@Configuration
+@EnableWebMvc
+public class WebConfig {
 
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                         .allowedOrigins("https://megaman-speedrun-81ua.vercel.app", "http://localhost:3000")
-//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT",
-//                                 "OPTIONS")
-//                         .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization",
-//                                 "Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
-//                                 "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials")
-//                         .exposedHeaders("Authorization")
-//                         .allowCredentials(true);
-//             }
-//         };
-//     }
-// }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://megaman-speedrun-81ua.vercel.app", "http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT",
+                                "OPTIONS")
+                        .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization",
+                                "Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
+                                "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials")
+                        .exposedHeaders("Authorization")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
